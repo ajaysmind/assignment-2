98eebdc7dd4513a0ae082c89330ce319fc0e3114
Ajay Jhala <97768591+ajaysmind@users.noreply.github.com>
2025-03-24 20:21:27 -0400

    Created and tested code for filling in all cells with selected color

--- script.js
+++ script.js
@@ -90,7 +90,10 @@ function fillU(){
 
 // Fill all cells
 function fillAll(){
-    alert("Clicked Fill All"); // Replace this line with your code.
+    let cells = table.getElementsByTagName("td");
+    for (let cell of cells){
+        cell.style.backgroundColor = colorSelected;
+    }
 }
 
 // Clear all cells
